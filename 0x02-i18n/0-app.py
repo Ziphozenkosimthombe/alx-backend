e71f95b948fa9bd8f06efcf9e0416a5d5a93dfba
Ziphozenkosimthombe <ziphomthombe478@gmail.com>
2024-04-09 16:00:12 +0000

    0-app.py

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
-
+"""A Basic Flask app.
+"""
 from flask import Flask, render_template
 
 
@@ -9,8 +10,10 @@ app.url_map.strict_slashes = False
 
 @app.route('/')
 def get_index() -> str:
+    """The home/index page.
+    """
     return render_template('0-index.html')
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port="5000")
+    app.run(host='0.0.0.0', port=5000)
