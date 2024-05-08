55adc4810f0a51eb443f9eaf3f33f254fe892f4c
Ziphozenkosimthombe <ziphoncayiyana@gmail.com>
2024-05-08 14:50:23 +0000

    5-subscriber.js

---
+++ 0x03-queuing_system_in_js/5-subscriber.js
@@ -0,0 +1,20 @@
+#!/usr/bin/env node
+import { createClient } from 'redis';
+
+const client = createClient()
+
+client.on('error', err => console.log(`Redis client not connected to the server: ${err}`));
+
+client.on('connect', () => {
+    console.log('Redis client connected to the server');
+});
+
+client.subscribe('holberton school channel')
+
+client.on('message', (err, msg) => {
+    console.log(msg);
+    if (msg === 'EXT_MSG'){
+        client.unsubstrube();
+        client.quit();
+    }
+});
